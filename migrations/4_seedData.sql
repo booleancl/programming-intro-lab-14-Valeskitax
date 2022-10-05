430b9612198e0277a5f03c02a2e908423e532468
val <ahumada.gonzalez.valeska@gmail.com>
2022-10-05 20:37:06 -0300

    querie 5

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,9 @@
-
+COPY users 
+FROM '/Users/val/IAD/programming-intro-lab-14-Valeskitax/data/users.csv'
+DELIMITER ',' CSV HEADER;
+COPY posts
+FROM '/Users/val/IAD/programming-intro-lab-14-Valeskitax/data/posts.csv'
+DELIMITER ',' CSV HEADER;
+COPY comments 
+FROM '/Users/val/IAD/programming-intro-lab-14-Valeskitax/data/comentarios.csv'
+DELIMITER ',' CSV HEADER;
--- queries/5_posts-with-users.sql
+++ queries/5_posts-with-users.sql
@@ -1,1 +1,3 @@
 /* Seleccionar el correo del usuario, el id del post y el título de los posts */
+SELECT p.id , p.title , u.email
+FROM posts p INNER JOIN users u ON u.id = p.users_id;
