d0009e5497e309af8a1f17c5ad6176686c1ce85e
val <ahumada.gonzalez.valeska@gmail.com>
2022-10-05 21:17:53 -0300

    querie 6

--- queries/6_comments-with-users.sql
+++ queries/6_comments-with-users.sql
@@ -1,7 +1,15 @@
 /*
 Seleccionar el correo, id del usuario y el contenido de los comentarios
 */
-SELECT email, users.id, content
-FROM comments
-INNER JOIN users ON users.id = comments.user_id 
-WHERE users.email != 'usuario06@hotmail.com'
+-- SELECT email, users.id, content
+-- FROM comments
+-- INNER JOIN users ON users.id = comments.user_id 
+-- WHERE users.email != 'usuario06@hotmail.com';
+
+SELECT u.email, u.id, c.content
+FROM comments c INNER JOIN users u 
+ON c.users_id = u.id;
+
+SELECT users.email, users.id, comments.content
+FROM comments INNER JOIN users 
+ON comments.users_id = users.id;
