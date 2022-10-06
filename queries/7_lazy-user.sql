1bc43f856e48fe5f66c7975c6ef0de9316c549cb
val <ahumada.gonzalez.valeska@gmail.com>
2022-10-05 21:30:15 -0300

    queries 7

--- queries/7_lazy-user.sql
+++ queries/7_lazy-user.sql
@@ -1,4 +1,7 @@
 /*
 Seleccionar el id y el correo del usuario que no tenga ningún post
 */
+select us.email,us.id from posts ps right join users us
+on us.id = ps.users_id 
+where ps.users_id is null;
 
